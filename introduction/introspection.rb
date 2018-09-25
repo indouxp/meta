9a82ba788995230b94c8d210f5598aa661ac1867
Tatsuo Indou <indou.tsystem@gmail.com>
2018-09-26 01:01:52 +0900

    update introspection.rb

--- introduction/introspection.rb
+++ introduction/introspection.rb
@@ -1,3 +1,7 @@
+# coding:utf-8
+# メタプログラミングruby
+# P.23
+#
 class Greeting
   def initialize(text)
     @text = text
@@ -10,6 +14,6 @@ end
 
 my_object = Greeting.new("Hello")
 
-p my_object.class
-p my_object.class.instance_methods(false)
-p my_object.instance_variables
+p my_object.class                         # => my_objectのクラスを聞く
+p my_object.class.instance_methods(false) # => my_objectの継承したものでないインスタンスメソッド
+p my_object.instance_variables            # => my_objectのインスタンス変数
